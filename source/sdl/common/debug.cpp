302ea99d863ddae46cdc49f431987afc612677e2
Robin Southern <betajaen@users.noreply.github.com>
2022-06-05 07:41:52 +0100

    debug write implementation for SDL

--- source/sdl/common/debug.cpp
+++ source/sdl/common/debug.cpp
@@ -100,11 +100,47 @@ namespace common
 
 	}
 
-	void debug_stop(const char* message) {
+	void debug_stop(const char* file, uint32 line, const char* message) {
+		fprintf(stdout, "%s:%u\n", file, line);
 		if (message) {
 			fputs(message, stdout);
 		}
 		exit(0);
+	}
+
+	void debug_write_test_fail_location(const char* file, uint32 line, const char* test_name) {    
+		fprintf(stdout, "%s failed\n:\n\t%s:%u\n", test_name, file, line);
+	}
+
+    void debug_write_str(const char* str_value) {
+		fputs(str_value, stdout);
+	}
+
+    void debug_write_char(byte byte_value) {
+		fprintf(stdout, "%2x", (uint32) byte_value);
+	}
+
+    void debug_write_char(char char_value) {
+		fputc(char_value, stdout);
+	}
+
+    void debug_write_unsigned_int(uint32 uint32_value) {
+		fprintf(stdout, "%u", uint32_value);
+	}
+
+    void debug_write_int(int32 int32_value) {
+		fprintf(stdout, "%d", int32_value);
+	}
+
+    void debug_write_bool(bool bool_value) {
+		if (bool_value)
+			fprintf(stdout, "true");
+		else
+			fprintf(stdout, "false");
+	}
+
+	void debug_write_pointer(const void* ptr) {
+		fprintf(stdout, "%p", ptr);
 	}
 
 }
