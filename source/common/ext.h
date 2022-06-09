9338c5daed894f0f1b513bb3ee41e20e0afdb729
Robin Southern <betajaen@users.noreply.github.com>
2022-06-09 17:32:22 +0100

    Added equals function to ext

--- source/common/ext.h
+++ source/common/ext.h
@@ -24,80 +24,123 @@ namespace common
 {
 	template<typename T>
 	inline void swap(T& l, T& r) {
-        T t = l;
-        l = r;
-        r = t;
-    }
+		T t = l;
+		l = r;
+		r = t;
+	}
 
-    template<typename T>
-    struct defaultValue {
-        static T value();
-    };
+	template<typename T>
+	struct defaultValue {
+		static T value();
+	};
 
-    template<> struct defaultValue<bool> { 
-        static inline bool value() { 
-            return false; 
-        }
-    };
-    
-    template<> struct defaultValue<int8> { 
-        static inline int8 value() { 
-            return 0;
-        }
-    };
-    
-    template<> struct defaultValue<int16> { 
-        static inline int16 value() { 
-            return 0;
-        }
-    };
-    
-    template<> struct defaultValue<int32> { 
-        static inline int32 value() { 
-            return 0;
-        }
-    };
-    
-    template<> struct defaultValue<uint8> { 
-        static inline uint8 value() { 
-            return 0;
-        }
-    };
-    
-    template<> struct defaultValue<uint16> { 
-        static inline uint16 value() { 
-            return 0;
-        }
-    };
-    
-    template<> struct defaultValue<uint32> { 
-        static inline uint32 value() { 
-            return 0;
-        }
-    };
+	template<> struct defaultValue<bool> { 
+		static inline bool value() { 
+			return false; 
+		}
+	};
+	
+	template<> struct defaultValue<int8> { 
+		static inline int8 value() { 
+			return 0;
+		}
+	};
+	
+	template<> struct defaultValue<int16> { 
+		static inline int16 value() { 
+			return 0;
+		}
+	};
+	
+	template<> struct defaultValue<int32> { 
+		static inline int32 value() { 
+			return 0;
+		}
+	};
+	
+	template<> struct defaultValue<uint8> { 
+		static inline uint8 value() { 
+			return 0;
+		}
+	};
+	
+	template<> struct defaultValue<uint16> { 
+		static inline uint16 value() { 
+			return 0;
+		}
+	};
+	
+	template<> struct defaultValue<uint32> { 
+		static inline uint32 value() { 
+			return 0;
+		}
+	};
 
-    template<typename T>
-    struct defaultValue<T*> {
-        static inline T* value() {
-            return NULL;
-        } 
-    };
+	template<typename T>
+	struct defaultValue<T*> {
+		static inline T* value() {
+			return NULL;
+		} 
+	};
 
-    template<typename T>
-    struct defaultValue<T**> {
-        static inline T** value() {
-            return NULL;
-        } 
-    };
+	template<typename T>
+	struct defaultValue<T**> {
+		static inline T** value() {
+			return NULL;
+		} 
+	};
 
-    class nocopy {
-        public:
-            nocopy() {}
-            ~nocopy() {}
-        private:
-            nocopy(const nocopy&);
-            const nocopy& operator=(const nocopy&);
-    };
+	class nocopy {
+		public:
+			nocopy() {}
+			~nocopy() {}
+		private:
+			nocopy(const nocopy&);
+			const nocopy& operator=(const nocopy&);
+	};
+
+	inline bool equals(const uint8& lhs, const uint8& rhs) {
+		return lhs == rhs;
+	}
+
+	inline bool equals(const uint16& lhs, const uint16& rhs) {
+		return lhs == rhs;
+	}
+
+	inline bool equals(const uint32& lhs, const uint32& rhs) {
+		return lhs == rhs;
+	}
+
+	inline bool equals(const int8& lhs, const int8& rhs) {
+		return lhs == rhs;
+	}
+
+	inline bool equals(const int16& lhs, const int16& rhs) {
+		return lhs == rhs;
+	}
+
+	inline bool equals(const int32& lhs, const int32& rhs) {
+		return lhs == rhs;
+	}
+
+	inline bool equals(const bool& lhs, const bool& rhs) {
+		return lhs == rhs;
+	}
+
+	inline bool equals(const char* lhs, const char* rhs) {
+		if ((lhs == NULL && rhs == NULL) || (lhs == rhs))
+			return true;
+		while (true) {
+			if (*lhs != *rhs)
+				return false;
+
+			if (*lhs == '\0')
+				return true;
+
+			lhs++;
+			rhs++;
+		}
+	}
 
 }
 
