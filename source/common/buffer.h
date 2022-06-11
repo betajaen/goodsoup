3b3199ea055ab70d744938c7dab7424c84680719
Robin Southern <betajaen@users.noreply.github.com>
2022-06-11 17:05:31 +0100

    Amiga/SDL Buffer Index is a template param

--- source/common/buffer.h
+++ source/common/buffer.h
@@ -25,12 +25,12 @@
 namespace common
 {
 
-	template<typename T>
+	template<typename T, typename Index = uint16>
 	struct Buffer
 	{
 	private:
 		T* _data;
-		uint32 _size;
+		Index _size;
 	public:
 
 		Buffer() {
@@ -42,7 +42,7 @@ namespace common
 			release();
 		}
 
-		void setSize(uint32 size, int flags = MEMF_CLEAR) {
+		void setSize(Index size, int flags = MEMF_CLEAR) {
 			if (_size && _data) {
 				releaseMemory(_data);;
 			}
@@ -70,7 +70,7 @@ namespace common
 
 		void callCtors() {
 			if (_size && _data) {
-				for (uint32 i = 0; i < _size; i++) {
+				for (Index i = 0; i < _size; i++) {
 					T* obj = ptr(i);
 					new (obj)T();
 				}
@@ -79,28 +79,28 @@ namespace common
 
 		void callDtors() {
 			if (_size && _data) {
-				for (uint32 i = 0; i < _size; i++) {
+				for (Index i = 0; i < _size; i++) {
 					_data[i].~T();
 				}
 			}
 		}
 
-		uint32 getSize() const {
+		Index getSize() const {
 			return _size;
 		}
 
-		void set(uint32 idx, const T& newValue) {
+		void set(Index idx, const T& newValue) {
 			if (idx >= _size)
 				error(GS_THIS, "array(Out of bounds, replace, %d)", idx);
 
 			_data[idx] = newValue;
 		}
 
-		void set_unchecked(uint32 idx, const T& newValue) {
+		void set_unchecked(Index idx, const T& newValue) {
 			_data[idx] = newValue;
 		}
 
-		T* ptr(uint32 idx) {
+		T* ptr(Index idx) {
 
 			if (idx >= _size)
 				error(GS_THIS, "array(Out of bounds, ptr, %d)", idx);
@@ -108,7 +108,7 @@ namespace common
 			return &_data[idx];
 		}
 
-		const T* ptr(uint32 idx) const {
+		const T* ptr(Index idx) const {
 
 			if (idx >= _size)
 				error(GS_THIS, "array(Out of bounds, const ptr, %d)", idx);
@@ -117,25 +117,25 @@ namespace common
 		}
 
 
-		T& get(uint32 idx) {
+		T& get(Index idx) {
 			if (idx >= _size)
 				error(GS_THIS, "array(Out of bounds, mut, %d)", idx);
 
 			return _data[idx];
 		}
 
-		const T& get(uint32 idx) const {
+		const T& get(Index idx) const {
 			if (idx >= _size)
 				error(GS_THIS, "array(Out of bounds, const, %d)", idx);
 
 			return _data[idx];
 		}
 
-		T& get_unchecked(uint32 idx) {
+		T& get_unchecked(Index idx) {
 			return _data[idx];
 		}
 
-		const T& get_unchecked(uint32 idx) const {
+		const T& get_unchecked(Index idx) const {
 			return _data[idx];
 		}
 	};
