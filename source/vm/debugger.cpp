870a5b24b22d42166e3d176e18328c137d4bea70
Robin Southern <betajaen@users.noreply.github.com>
2022-07-16 20:12:43 +0100

    VM Debugger can be filtered via preprocessor.

--- source/vm/debugger.cpp
+++ source/vm/debugger.cpp
@@ -21,7 +21,13 @@
 #include "vm.h"
 #include "../debug.h"
 
-#define GS_VM_DEBUG_TO_LOG 1
+#define GS_VM_DEBUG_COMMENTS 1
+#define GS_VM_DEBUG_SCRIPT 1
+#define GS_VM_DEBUG_OPCODE 1
+#define GS_VM_DEBUG_STACK 1
+#define GS_VM_DEBUG_LOCALS 1
+#define GS_VM_DEBUG_RESULTS 1
+#define GS_VM_DEBUG_TO_LOG 0
 
 extern const char GOODSOUP_VERSION_STR[];
 
@@ -29,6 +35,8 @@ namespace gs
 {
 
 #if defined(GS_VM_DEBUG) && GS_VM_DEBUG==1
+
+	const char* getOpcodeString(uint8 opcode);
 
     StringAppendFile sDebugFile;
     bool sPaused;
@@ -70,21 +78,26 @@ namespace gs
     }
 
     void vmDebugRemark(const char* comment) {
+#if GS_VM_DEBUG_COMMENTS == 1
         if (sPaused == false) {
             sDebugFile.writeFormat("REM \"%s\"\n", comment);
         }
+#endif
     }
 
     void vmDebugScript(uint8 context, uint16 scriptNum, uint8 scriptType, uint8 verb, uint32 checkSum) {
+#if GS_VM_DEBUG_SCRIPT == 1
         if (sPaused == false) {
             sDebugFile.writeFormat("\n@ %02lx %04lx \"%s\" %02lx %08lx\n", context, scriptNum, ObjectWhereToString(scriptType), verb, checkSum);
 #if GS_VM_DEBUG_TO_LOG == 1
 			debug(GS_THIS, "@ %02lx %04lx \"%s\" %02lx %08lx", context, scriptNum, ObjectWhereToString(scriptType), verb, checkSum)	;
 #endif
         }
+#endif
     }
 
     void vmDebugLocals(uint8 count, int32* values) {
+#if GS_VM_DEBUG_LOCALS == 1
         if (sPaused == false) {
             for(uint8 i = 0;i < count;i++) {
 				if (values[i] != 0) {
@@ -95,9 +108,11 @@ namespace gs
 				}
             }
         }
+#endif
     }
 
     void vmDebugStack(uint8 count, int32* values) {
+#if GS_VM_DEBUG_STACK == 1
          if (sPaused == false) {
             for(uint8 i = 0;i < count;i++) {
                 sDebugFile.writeFormat("S %02x %08x\n", i, values[i]);
@@ -106,60 +121,73 @@ namespace gs
 #endif
             }
         }
+#endif
     }
 
     void vmDebugOpcode(uint16 pc, uint8 opcode) {
+#if GS_VM_DEBUG_OPCODE == 1
         if (sPaused == false) {
-            sDebugFile.writeFormat("> %04lx %02lx \"%s\"\n", pc, opcode, OpcodeToString(opcode));
+            sDebugFile.writeFormat("> %04lx %02lx \"%s\"\n", pc, opcode, getOpcodeString(opcode));
 #if GS_VM_DEBUG_TO_LOG == 1
-			debug(GS_THIS, "> %04lx %02lx \"%s\"", pc, opcode, OpcodeToString(opcode));
+			debug(GS_THIS, "> %04lx %02lx \"%s\"", pc, opcode, getOpcodeString(opcode));
 #endif
         }
+#endif
     }
 
     void vmDebugResult(int32 v0) {
+#if GS_VM_DEBUG_RESULTS == 1
         if (sPaused == false) {
             sDebugFile.writeFormat(". %08lx\n", v0);
 #if GS_VM_DEBUG_TO_LOG == 1
 			debug(GS_THIS, ". %08lx", v0);
 #endif
         }
+#endif
     }
 
     void vmDebugResult(int32 v0, int32 v1) {
+#if GS_VM_DEBUG_RESULTS == 1
         if (sPaused == false) {
             sDebugFile.writeFormat(". %08lx %08lx\n", v0, v1);
 #if GS_VM_DEBUG_TO_LOG == 1
 			debug(GS_THIS, ". %08lx %08lx", v0, v1);
 #endif
         }
+#endif
     }
 
     void vmDebugResult(int32 v0, int32 v1, int32 v2) {
+#if GS_VM_DEBUG_RESULTS == 1
         if (sPaused == false) {
             sDebugFile.writeFormat(". %08lx %08lx %08lx\n", v0, v1, v2);
 #if GS_VM_DEBUG_TO_LOG == 1
 			debug(GS_THIS, ". %08lx %08lx %08lx", v0, v1, v2);
 #endif
         }
+#endif
     }
 
     void vmDebugResult(int32 v0, int32 v1, int32 v2, int32 v3) {
+#if GS_VM_DEBUG_RESULTS == 1
         if (sPaused == false) {
             sDebugFile.writeFormat(". %08lx %08lx %08lx %08lx\n", v0, v1, v2, v3);
 #if GS_VM_DEBUG_TO_LOG == 1
 			debug(GS_THIS, ". %08lx %08lx %08lx %08lx", v0, v1, v2, v3);
 #endif
         }
+#endif
     }
 
     void vmDebugResult(int32 v0, int32 v1, int32 v2, int32 v3, int32 v4) {
+#if GS_VM_DEBUG_RESULTS == 1
         if (sPaused == false) {
             sDebugFile.writeFormat(". %08lx %08lx %08lx %08lx %08lx\n", v0, v1, v2, v3, v4);
 #if GS_VM_DEBUG_TO_LOG == 1
 			debug(GS_THIS, ". %08lx %08lx %08lx %08lx %08lx", v0, v1, v2, v3, v4);
 #endif
         }
+#endif
     }
 #endif
 }
